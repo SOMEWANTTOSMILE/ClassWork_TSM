8705ca3d91c7c05f0e5ca9efc9796c75877601a1
Somewanttosmile <149158489+SOMEWANTTOSMILE@users.noreply.github.com>
2024-01-24 21:39:29 +0300

    Delete lesson_24 directory

--- lesson_24/authentication/test.py
+++
@@ -1,9 +0,0 @@
-def hide():
-    srting_ = "pivo@gmail.com"
-    split_mail = str(srting_).split('@')
-    a = split_mail[0][2]
-    b = split_mail[1]
-    print(a, 'split', b)
-
-
-print(hide())
--- lesson_24/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Main_sait, Sign_up, Sing_in
-
-urlpatterns = [
-    path('', Main_sait.as_view()),
-    path('sign_up/', Sign_up.as_view()),
-    path('sign_in/', Sing_in.as_view())
-]
